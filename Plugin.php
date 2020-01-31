7a2af9dbec2677eda7a589adac66116575f055fc
Flynsarmy <flynsarmy@gmail.com>
2020-01-31 06:10:46 +0000

    Apply fixes from StyleCI

--- Plugin.php
+++ Plugin.php
@@ -1,10 +1,12 @@
-<?php namespace Flynsarmy\CommentableHtml;
+<?php
+
+namespace Flynsarmy\CommentableHtml;
 
 use Event;
 use System\Classes\PluginBase;
 
 /**
- * Commentable Plugin Information File
+ * Commentable Plugin Information File.
  */
 class Plugin extends PluginBase
 {
@@ -21,7 +23,7 @@ class Plugin extends PluginBase
             'name'        => 'Commentable - HTML Comments Extension',
             'description' => 'Allows comments to be made with a WYSIWYG editor.',
             'author'      => 'Flynsarmy',
-            'icon'        => 'icon-comments'
+            'icon'        => 'icon-comments',
         ];
     }
 
@@ -36,46 +38,57 @@ class Plugin extends PluginBase
     {
         $useHtml = \Flynsarmy\Commentable\Models\Settings::instance()->get('enable_html_comments', false);
 
-        if ( $useHtml )
-        {
-            \Flynsarmy\Commentable\Models\Comment::extend(function($model) {
-                $model->bindEvent('model.getAttribute', function($attribute, $value) use ($model) {
-                    if ( $attribute == 'renderedContent' )
+        if ($useHtml) {
+            \Flynsarmy\Commentable\Models\Comment::extend(function ($model) {
+                $model->bindEvent('model.getAttribute', function ($attribute, $value) use ($model) {
+                    if ($attribute == 'renderedContent') {
                         return htmlspecialchars_decode($model->content);
+                    }
 
                     return null;
                 });
             });
 
             // Convert backend to content field to a richeditor
-            Event::listen('backend.form.extendFieldsBefore', function(\Backend\Widgets\Form $form) {
+            Event::listen('backend.form.extendFieldsBefore', function (\Backend\Widgets\Form $form) {
                 if (
                     !$form->getController() instanceof \Flynsarmy\Commentable\Controllers\Threads &&
                     !$form->getController() instanceof \Flynsarmy\Commentable\Controllers\Notifications
-                )
+                ) {
                     return;
-                if ( !$form->model instanceof \Flynsarmy\Commentable\Models\Comment ) return;
-                if ( !in_array($form->getContext(), ['update', 'edit']) ) return;
+                }
+                if (!$form->model instanceof \Flynsarmy\Commentable\Models\Comment) {
+                    return;
+                }
+                if (!in_array($form->getContext(), ['update', 'edit'])) {
+                    return;
+                }
 
-                if ( isset($form->fields['content']) )
+                if (isset($form->fields['content'])) {
                     $form->fields['content']['type'] = 'richeditor';
+                }
             });
         }
 
         // Add setting to toggle HTML forms on and off
-        Event::listen('backend.form.extendFieldsBefore', function(\Backend\Widgets\Form $form) {
+        Event::listen('backend.form.extendFieldsBefore', function (\Backend\Widgets\Form $form) {
             if (
                 !$form->getController() instanceof \Flynsarmy\Commentable\Controllers\Settings
-            )
+            ) {
                 return;
-            if ( !$form->model instanceof \Flynsarmy\Commentable\Models\Settings ) return;
-            if ( !in_array($form->getContext(), ['update', 'edit']) ) return;
+            }
+            if (!$form->model instanceof \Flynsarmy\Commentable\Models\Settings) {
+                return;
+            }
+            if (!in_array($form->getContext(), ['update', 'edit'])) {
+                return;
+            }
 
             $form->tabs['fields']['enable_html_comments'] = [
-                'label' => 'Enable HTML comments?',
-                'type' => 'checkbox',
-                'comment' => "Comments are edited in a WYSIWYG editor and full HTML markup displayed",
-                'tab' => 'General',
+                'label'   => 'Enable HTML comments?',
+                'type'    => 'checkbox',
+                'comment' => 'Comments are edited in a WYSIWYG editor and full HTML markup displayed',
+                'tab'     => 'General',
             ];
         });
     }
--- components/HtmlComments.php
+++ components/HtmlComments.php
@@ -1,4 +1,6 @@
-<?php namespace Flynsarmy\CommentableHtml\Components;
+<?php
+
+namespace Flynsarmy\CommentableHtml\Components;
 
 use Cms\Classes\ComponentBase;
 use Flynsarmy\Commentable\Models\Settings;
@@ -11,14 +13,13 @@ class HtmlComments extends ComponentBase
     {
         return [
             'name'        => 'HTML Comments',
-            'description' => 'Converts Commentable comments to HTML'
+            'description' => 'Converts Commentable comments to HTML',
         ];
     }
 
     public function onRun()
     {
-        if ( $this->usingHtmlComments() )
-        {
+        if ($this->usingHtmlComments()) {
             $this->addCss('https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.20.0/ui/trumbowyg.min.css');
             $this->addJs('https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.20.0/trumbowyg.min.js');
             $this->addJs('assets/js/script.js');
